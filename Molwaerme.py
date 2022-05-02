d15c27f4604b67011a507a890fa96b776bc5228c
Cihad <cihad.goezsuez@iaps.info>
2022-05-02 04:37:49 +0200

    Neue große Tabelle und erste Plots

--- Molwaerme.py
+++ Molwaerme.py
@@ -4,6 +4,12 @@ import uncertainties.unumpy as unp
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
 import csv 
+
+
+import seaborn as sns
+
+sns.set(style="darkgrid")
+sns.set_palette("magma")
 #Masse Probe in kg
 m = 0.342
 #Molmasse Kupfer in kg/mol
@@ -16,8 +22,9 @@ V = 7.11 * 10**-6
 R = 8.31439
 
 t, R_P, R_G, U, I = np.genfromtxt("daten_neu.csv", delimiter=",", skip_header=3, unpack=True,encoding='latin-1')
+anzahlDaten = t.size
 
-print(t, R_P, R_G, U, I )
+
 #Bei der Messung wurde der Komma falsch platziert. Dafür wird korrigiert
 R_P = R_P*0.1
 R_G = R_G*0.1
@@ -33,35 +40,48 @@ T = (T_P + T_G) / 2
 #alpha bestimmen mit T
 alpha = - ufloat(8.2, 0.7) * 10**-9 * T**4 + ufloat(7.4, 0.5) * 10**-6 * T**3 - ufloat(2.5, 0.1) * 10**-3 * T**2 + ufloat(0.41, 0.02) * T - ufloat(11.3, 0.6)
 
-print(alpha)
 
 #temporäre uArrays für C_p, C_v und T_plot erstellen
-err0 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-err1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+err0 = [0 for i in range(anzahlDaten)]
 C_p = unp.uarray(err0, err0)
 C_v = unp.uarray(err0, err0)
-T_plot = unp.uarray(err1, err1)
-C_v_plot = unp.uarray(err1, err1)
+T_plot = unp.uarray(err0, err0)
+C_v_plot = unp.uarray(err0, err0)
 
 #C_p bestimmen (Formel: U * I * delta_t * M / m / delta_T)
-for x in range(31):
+for x in range(anzahlDaten-1):
     C_p[x] = U[x] * I[x] * 10**-3 * (t[x+1] - t[x]) * 60 * M / m / (T[x+1] - T[x])
 
-print(C_p)
+
 
 #C_v bestimmen
-for x in range(31):
+for x in range(anzahlDaten-1):
     C_v[x] = C_p[x] - 9 * (alpha[x+1] * 10**-6)**2 * k * 10**9 * T[x+1] * V
 #    T_plot[x] = T[x+1]
 
-print(C_v)
 
+
+writeTable = np.array([t,np.around(R_P, decimals=3), np.around(R_G, decimals=3), np.around(unp.nominal_values(T_P), decimals=3), np.around(unp.nominal_values(T_G), decimals=3), np.around(unp.nominal_values(T), decimals=3), np.around(U, decimals=3), np.around(I, decimals=3), np.around(unp.nominal_values(C_p), decimals=3), np.around(unp.nominal_values(C_v), decimals=3)])
 with open('datendat-copy.csv', 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     writer.writerow(['t','R_P', 'R_Z', 'T_P', 'T_Z', 'T',  'U', 'I', 'C_p', 'C_V'])
-    rows = zip(t,R_P, R_G, unp.nominal_values(T_P), unp.nominal_values(T_G), unp.nominal_values(T), U, I, unp.nominal_values(C_p), unp.nominal_values(C_v))
+    rows = zip(*writeTable)
     writer.writerows(rows)
-
+with open('datendat-copy-cut1.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(['t','R_P', 'R_Z', 'T_P', 'T_Z', 'T',  'U', 'I', 'C_p', 'C_V'])
+    rows = zip(*writeTable[:,:31])
+    writer.writerows(rows)
+with open('datendat-copy-cut2.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(['t','R_P', 'R_Z', 'T_P', 'T_Z', 'T',  'U', 'I', 'C_p', 'C_V'])
+    rows = zip(*writeTable[:,31:62])
+    writer.writerows(rows)
+with open('datendat-copy-cut3.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(['t','R_P', 'R_Z', 'T_P', 'T_Z', 'T',  'U', 'I', 'C_p', 'C_V'])
+    rows = zip(*writeTable[:,62:])
+    writer.writerows(rows)
 #Die größten Ausreißer ausfiltern also Wert 4, 5, 21 raus aus den Arrays
 #Für die ungefilterte Version Zeile 51 verwenden an Stelle von dem hier und in Zeile 39 err1 durch err0 2 mal ersetzen und in Zeile 75 C_v_plot durch C_v ersetzen
 # for x in range(3):
@@ -77,8 +97,15 @@ with open('datendat-copy.csv', 'w') as csvfile:
 #     T_plot[x+18] = T[x+21]
 
 #3R Linie zu markieren
+
+C_v_plot = C_v[1:-3]
+T_plot = T[1:-3]
+
 x_plot = np.linspace(75, 310, 2)
 R_plot = [3 * R, 3 * R]
+
+
+
 
 def debye():
     return 0
@@ -86,9 +113,12 @@ def einstein():
     return 0
 #C_v gegen T plotten
 plt.plot(x_plot, R_plot, 'b-', label='3R', linewidth=1)
-plt.errorbar(unp.nominal_values(T_plot), unp.nominal_values(C_v_plot), yerr = unp.std_devs(C_v_plot), fmt='rx', label='Messwerte')
+plt.errorbar(unp.nominal_values(T_plot), unp.nominal_values(C_v_plot), yerr = unp.std_devs(C_v_plot),fmt = "k.", label='Messwerte')
 plt.xlabel(r'$T \, / \, \mathrm{K}$')
 plt.ylabel(r'$C_V \, / \, \mathrm{J} \, \mathrm{K}^{-1} \mathrm{mol}^{-1}$')
+
+plt.ylim(0,60)
 plt.legend()
 plt.tight_layout()
-plt.savefig('C_V_gefiltert.pdf')
+plt.grid()
+plt.savefig('build/C_V_gefiltert.pdf')
