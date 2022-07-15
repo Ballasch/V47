53d4c6889f4f9f020c830aab38a43d73fd5696e9
lydiavonderweiden <lydia.weiden@tu-dortmund.de>
2022-07-15 21:16:55 +0200

    Korrektur die zweite
    Sollte jetzt hoffentlich alles Richtig sein

--- Debye.py
+++ Debye.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
-import seaborn as sns 
+#import seaborn as sns 
 
-sns.set(style="darkgrid")
-sns.set_palette("magma")
+#sns.set(style="darkgrid")
+#sns.set_palette("magma")
 C_v, T, Theta_T, Theta_D= np.genfromtxt("C_v_und_T_neu.csv", delimiter=",", unpack=True)
 
 #print(C_v, T, Theta_T)
@@ -22,9 +22,38 @@ plt.clf()
 plt.close()
 
 
-plt.plot(T, Theta_D, 'k.')
+#Plot 2 versuchen auszufriemeln
+def gerade(x,a,m):
+    return a*x+m
+
+a = 2
+b =1
+
+T_oben = T[Theta_D > gerade(T,a,b)]
+T_unten = T[Theta_D < gerade(T,a,b)]
+
+Theta_oben = Theta_D[Theta_D > gerade(T,a,b)]
+Theta_unten = Theta_D[Theta_D < gerade(T,a,b)]
+
+params1, covariance_matrix = np.polyfit(T_oben, Theta_oben, deg=1, cov=True)
+errors1 = np.sqrt(np.diag(covariance_matrix))
+
+params2, covariance_matrix = np.polyfit(T_unten, Theta_unten, deg=1, cov=True)
+errors2 = np.sqrt(np.diag(covariance_matrix))
+
+plt.plot(T, gerade(T, *params1))
+plt.plot(T, gerade(T, *params2))
+print(params1)
+print(errors1)
+
+print(params2)
+print(errors2)
+
+
+plt.plot(T, Theta_D, 'k.', label='Messwerte')
 plt.xlabel(r'$T \, / \, \mathrm{K}$')
 plt.ylabel(r'$\theta_D \, / \, \mathrm{K}$')
+plt.legend()
 plt.tight_layout()
 plt.savefig('build/Theta_T.pdf')
 # #Mitteln
@@ -44,3 +73,4 @@ plt.savefig('build/Theta_T.pdf')
 # Debye = ufloat(Theta_D_gemittelt, Theta_D_gemittelt_err)
 
 # print(Debye)
+
--- Molwaerme.py
+++ Molwaerme.py
@@ -1,15 +1,14 @@
+import csv
+
 import matplotlib.pyplot as plt
 import numpy as np
+#import seaborn as sns
 import uncertainties.unumpy as unp
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
-import csv 
 
-
-import seaborn as sns
-
-sns.set(style="darkgrid")
-sns.set_palette("magma")
+#sns.set(style="darkgrid")
+#sns.set_palette("magma")
 #Masse Probe in kg
 m = 0.342
 #Molmasse Kupfer in kg/mol
@@ -23,40 +22,55 @@ R = 8.31439
 
 t, R_P, R_G, U, I = np.genfromtxt("daten_neu.csv", delimiter=",", skip_header=3, unpack=True,encoding='latin-1')
 anzahlDaten = t.size
+err0 = [0 for i in range(anzahlDaten)]
 
+Ablesen = [0.1 for i in range(anzahlDaten)]
 
 #Bei der Messung wurde der Komma falsch platziert. Dafür wird korrigiert
 R_P = R_P*0.1
 R_G = R_G*0.1
+#Wir wollen übrigens Ohm, nicht Kiloohm haben. Nicht vergessen nachfolgend in Code anzupassen
+R_P = R_P*1000
+R_G = R_G*1000
+
+R_P = unp.uarray(R_P, Ablesen)
+R_G = unp.uarray(R_G, Ablesen)
+
 #Widerstände in Temperatur umrechnen und in K umrechnen mit 0 als Fehler
-T_P = 0.00134 * (R_P * 1000)**2 + 2.296 * (R_P * 1000) - 243.02 + 273.15
-T_G = 0.00134 * (R_G * 1000)**2 + 2.296 * (R_G * 1000) - 243.02 + 273.15
-T_P = unp.uarray(T_P, T_P*0.002)
-T_G = unp.uarray(T_G, T_G*0.002)
+T_P = 0.00134 * R_P**2 + 2.296 * R_P - 243.02 + 273.15
+T_G = 0.00134 * R_G**2 + 2.296 * R_G  - 243.02 + 273.15
+
+#Wieso? Warum gehen wir nicht einfach davon aus das die Widerstände falsch abgelesen wurden?
+#T_P = unp.uarray(T_P, T_P*0.002)
+#T_G = unp.uarray(T_G, T_G*0.002)
+
 #Mittlere Temperatur zwischen Gefäß und Probe bestimmen
 T = (T_P + T_G) / 2
 
 
 #Mit Fehler für die Berechnung von C_P
 #T2 = unp.uarray(T,0.002*T )
-U2 = unp.uarray(U, U*0.1)
-I2 = unp.uarray(I, I*0.1)
+U2 = unp.uarray(U, Ablesen)
+I2 = unp.uarray(I, Ablesen)
 t2 = unp.uarray(t, 5)
 
 #alpha bestimmen mit T
-alpha = - ufloat(8.2, 0.7) * 10**-9 * T**4 + ufloat(7.4, 0.5) * 10**-6 * T**3 - ufloat(2.5, 0.1) * 10**-3 * T**2 + ufloat(0.41, 0.02) * T - ufloat(11.3, 0.6)
-
+#alpha = - ufloat(8.2, 0.7) * 10**-9 * unp.nominal_values(T)**4 + ufloat(7.4, 0.5) * 10**-6 * unp.nominal_values(T)**3 - ufloat(2.5, 0.1) * 10**-3 * unp.nominal_values(T)**2 + ufloat(0.41, 0.02) * unp.nominal_values(T) - ufloat(11.3, 0.6)
+alpha = - 8.2 * 10**-9 * unp.nominal_values(T)**4 + 7.4 * 10**-6 * unp.nominal_values(T)**3 - 2.5 * 10**-3 * unp.nominal_values(T)**2 +0.41 * unp.nominal_values(T) - 11.3
 
 #temporäre uArrays für C_p, C_v und T_plot erstellen
-err0 = [0 for i in range(anzahlDaten)]
+
 C_p = unp.uarray(err0, err0)
 C_v = unp.uarray(err0, err0)
-T_plot = unp.uarray(err0, err0)
-C_v_plot = unp.uarray(err0, err0)
+#T_plot = unp.uarray(err0, err0)
+#C_v_plot = unp.uarray(err0, err0)
+
+#Zeitdifferenz ist ein fester Wert für uns
+t_diff = 1.5
 
 #C_p bestimmen (Formel: U * I * delta_t * M / m / delta_T)
 for x in range(anzahlDaten-1):
-    C_p[x] = U[x] * I[x] * 10**-3 * (t[x+1] - t[x]) * 60 * M / m / (T_P[x+1] - T_P[x])
+    C_p[x] = U2[x] * I2[x] * 10**-3 * t_diff * 60 * M / m / (T_P[x+1] - T_P[x])
 
 
 
@@ -67,8 +81,7 @@ for x in range(anzahlDaten-1):
 #    T_plot[x] = T[x+1]
 
 
-
-writeTable = np.array([t,np.around(R_P, decimals=4), np.around(R_G, decimals=4), np.around(unp.nominal_values(T_P), decimals=3), np.around(unp.nominal_values(T_G), decimals=3), np.around(unp.nominal_values(T), decimals=3), np.around(U, decimals=3), np.around(I, decimals=3), np.around(unp.nominal_values(C_p), decimals=3), np.around(unp.nominal_values(C_v), decimals=3)])
+writeTable = np.array([t,np.around(unp.nominal_values(R_P), decimals=4), np.around(unp.nominal_values(R_G), decimals=4), np.around(unp.nominal_values(T_P), decimals=3), np.around(unp.nominal_values(T_G), decimals=3), np.around(unp.nominal_values(T), decimals=3), np.around(U, decimals=3), np.around(I, decimals=3), np.around(unp.nominal_values(C_p), decimals=3), np.around(unp.nominal_values(C_v), decimals=3)])
 with open('datendat-copy.csv', 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     writer.writerow(['t','R_P', 'R_Z', 'T_P', 'T_Z', 'T',  'U', 'I', 'C_p', 'C_V'])
@@ -105,8 +118,8 @@ with open('datendat-copy-cut3.csv', 'w') as csvfile:
 
 #3R Linie zu markieren
 
-C_v_plot = C_v[1:-3]
-T_plot = T[1:-3]
+C_v_plot = C_v[1:-1]
+T_plot = T[1:-1]
 
 x_plot = np.linspace(75, 310, 2)
 R_plot = [3 * R, 3 * R]
@@ -124,7 +137,7 @@ plt.errorbar(unp.nominal_values(T_plot), unp.nominal_values(C_v_plot), yerr = un
 plt.xlabel(r'$T \, / \, \mathrm{K}$')
 plt.ylabel(r'$C_V \, / \, \mathrm{J} \, \mathrm{K}^{-1} \mathrm{mol}^{-1}$')
 
-plt.ylim(0,60)
+plt.ylim(10,40)
 plt.legend()
 plt.tight_layout()
 plt.grid()
@@ -133,15 +146,19 @@ plt.savefig('build/C_V_gefiltert.pdf')
 plt.clf()
 plt.close()
 
+
 C_v_plot = C_v
 T_plot = T
 plt.plot(x_plot, R_plot, 'b-', label='3R', linewidth=1)
-plt.errorbar(unp.nominal_values(T_plot), unp.nominal_values(C_v_plot), yerr = unp.std_devs(C_v_plot),fmt = "k.", label='Messwerte')
+plt.errorbar(unp.nominal_values(T_plot), unp.nominal_values(C_v_plot), 
+             yerr = unp.std_devs(C_v_plot),fmt = "k.", elinewidth=0.5, label='Messwerte')
 plt.xlabel(r'$T \, / \, \mathrm{K}$')
 plt.ylabel(r'$C_V \, / \, \mathrm{J} \, \mathrm{K}^{-1} \mathrm{mol}^{-1}$')
 
-plt.ylim(0,60)
-plt.legend()
+#plt.ylim(10,32)
+plt.ylim(10,32)
+plt.xlim(75,310)
+plt.legend(loc='lower right')
 plt.grid()
 plt.tight_layout()
 
